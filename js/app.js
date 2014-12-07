72dfccb6bf66a1e452ea7d903f1a9d4c870d264e
Melanie Levy-Hara <melanie.levy.hara@gmail.com>
2014-12-07 17:48:54 -0500

    ui-router
    Trying to implement ui-router with angular.

--- js/app.js
+++ js/app.js
@@ -14,5 +14,33 @@
   });
 
 })();
+// (function(){
+//   var myApp = angular.module('myApp', ['ui.router']);
+//   myApp.config(function($stateProvider, $urlRouterProvider) {
+//     //
+//     // For any unmatched url, redirect to /state1
+//     $urlRouterProvider.otherwise("/state1");
+//     //
+//     // Now set up the states
+//     $stateProvider
+//       .state('state1', {
+//         url: "/state1",
+//         templateUrl: "Homepage.html"
+//       })
+      
+//       .state('state2', {
+//         url: "/state2",
+//         templateUrl: "AskMe.html"
+//       })
+      
+//       .state('state3', {
+//         url: "/state3",
+//         templateUrl: "Resume.html"
+//       })
 
-  
+//       .state('state4', {
+//         url: "/state4",
+//         templateUrl: "Contact.html"
+//       })
+//   });
+// })();
